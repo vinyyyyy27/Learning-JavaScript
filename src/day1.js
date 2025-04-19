4b32251afe836e153acf1f84de988067520fd7f1
vinyyyyy27 <vin092705@gmail.com>
2025-04-19 12:50:38 +0800

    day 1 of learning JS

---
+++ src/day1.js
@@ -0,0 +1,10 @@
+//Declare two string variables, hello and world, and concatenate them to say "Hello World"
+
+
+let Firtsname;
+let Lastname;
+Firtsname = 'Marvin';
+Lastname = 'Caritativo';
+console.log(Firtsname + " "+  Lastname);
+
+
--- src/helloworld.js
+++ src/helloworld.js
@@ -1,1 +1,7 @@
-console.log("Hello, World!");
+
+/*console.log("Hello, World!");
+console.log('i love U');
+window.alert('warning!!!!!!');
+window.alert('hello matherfucker');*/
+document.getElementById("myh1").textContent = 'hello';
+document.getElementById("p").textContent =  'hello i want pizza';
